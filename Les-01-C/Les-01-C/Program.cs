204fa84e13b02fe4166c4dc41a5f14c30f4d9a1e
Monique Wienholds <snuffelien@gmail.com>
2021-02-09 15:43:37 +0100

    eerste opdracht

---
+++ Les-01-C/Les-01-C/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading;
+
+namespace Les_01_C
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            string text = "Hello World";
+            foreach (char c in text)
+            {
+                Console.Write(c);
+                Thread.Sleep(70);
+            }
+        }
+    }
+}
